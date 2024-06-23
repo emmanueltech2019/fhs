95858c61e56c6f5e05d8d6fcc15cacbf9a966f22
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-23 08:03:22 +0100

    first commit

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -13,7 +13,6 @@ const data:Data ={
     domain: "fortlauderdaleherniaspecialists.com",
     logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719124349/FHS_o9fpk5.png',
     src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114614.47826903533!2d-80.22796912115977!3d26.141243174568594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9012720facaf5%3A0x7602be7540bf8ebe!2sFort%20Lauderdale%2C%20FL%2C%20USA!5e0!3m2!1sen!2sng!4v1719126007313!5m2!1sen!2sng"
-
 }
 
 
